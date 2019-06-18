3b33da49705f283bc6f4fb96d3e8389f11e26aa3
ChristinaPatina <47825387+ChristinaPatina@users.noreply.github.com>
2019-06-18 13:29:38 +0300

    Update models.py

--- weather/models.py
+++ weather/models.py
@@ -7,8 +7,8 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
-    def __eq__(self, other):
-    	return self.name == other.name
+    #def __eq__(self, other):
+    	#return self.name == other.name
 
-    def __hash__(self):
-      return hash(self.name)
+    #def __hash__(self):
+      #return hash(self.name)
