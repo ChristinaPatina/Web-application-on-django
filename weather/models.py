51426d05dc0048a161e28fef7fee934285335b62
ChristinaPatina <47825387+ChristinaPatina@users.noreply.github.com>
2019-06-18 16:39:10 +0300

    Update models.py

--- weather/models.py
+++ weather/models.py
@@ -8,7 +8,23 @@ class City(models.Model):
         return self.name
 
     #def __eq__(self, other):
-    	#return self.name == other.name
+        #return self.name == other.name
 
     #def __hash__(self):
-      #return hash(self.name)
+        #return hash(self.name)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
