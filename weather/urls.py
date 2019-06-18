d1a195e6d97fbc54a66a2d020d40f3020d623144
ChristinaPatina <47825387+ChristinaPatina@users.noreply.github.com>
2019-06-18 17:48:21 +0300

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('weather_info/', views.index, name='weather_info'),
     path('vote/detail/', views.detail, name='detail'),
-    path('results/', views.results, name='results'),
+    path('vote/results/', views.results, name='results'),
     path('vote/', views.vote, name='vote'),
 ]
